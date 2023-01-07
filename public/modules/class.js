fbeb74534a5b06d159f4da9bac8cfc3e6c7347b2
Quent_DLM <qdelameilleure@gmail.com>
2023-01-07 14:56:37 +0100

    rage du guerrier

--- public/modules/class.js
+++ public/modules/class.js
@@ -11,9 +11,9 @@ class Boss {
             const phrases = ["Qu'est ce qui a quatre pates le matin deux le soir et trois a midi ?", "je suis le bien et le mal mais je n'ai pourtant aucun corps qui suis je ?", "Je commence la nuit et termine le matin qui suis je ?"];
             const index = Math.floor(Math.random() * phrases.length);
             const phrase = phrases[index];
-            
+
             console.log(phrase);
-            
+
         }
     }
 }
@@ -22,17 +22,35 @@ const Sauron = new Boss('Sauron', 1000, 50);
 const Chronos = new Boss('Chronos', 500, 75);
 const Lilith = new Boss('Lilith', 250, 100);
 
-Sauron.proposeEnigme(); 
-Chronos.proposeEnigme(); 
-Lilith.proposeEnigme(); 
+Sauron.proposeEnigme();
+Chronos.proposeEnigme();
+Lilith.proposeEnigme();
 
 //Héros//
-class Héros {
-    constructor(name, health, attack, rage) {
+class Guerrier {
+    constructor(name, health, attack) {
         this.name = name;
         this.health = health;
         this.attack = attack;
-        this.ragepoint = rage
+        this.rage = 0;
+    }
+
+    gagneRage() {
+        this.rage++;
+        if (this.rage >= 4) {
+            this.attack *= 1.25;
+            this.rage = 0;
+            setTimeout(() => {
+                this.attack /= 1.25;
+            }, 1000); 
+        }
+    }
+
+    defense() {
+
+    }
+
+    attaque() {
 
     }
 }
