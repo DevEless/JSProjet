f8ceb5b2ae8e59502ec196d40628d298f3f81e8e
Quent_DLM <qdelameilleure@gmail.com>
2023-01-14 13:22:38 +0100

    correction

--- public/main.js
+++ public/main.js
@@ -2,14 +2,14 @@ import {
 chosenBoss , calculateHealth , attack , personnages
 } from "./fonction.js"
 
+chooseRandomBoss()
+const chosenBoss = chooseRandomBoss();
 alert(`Vous allez affronter le grand ${chosenBoss.name}.`);
 
 alert(`Le sort est cruel, quels sera le nom de vos trois Héros légendaire ?`);
 
 console.log(personnages);
 
-chooseRandomBoss()
-const chosenBoss = chooseRandomBoss();
 
 while (chosenBoss.health > 0 && (personnages[0].health > 0 || personnages[1].health > 0 || personnages[2].health > 0) ){
     if (personnages[0].health > 0 ) {
